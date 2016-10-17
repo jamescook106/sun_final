05bcff0e0ea6dff598d8ccd6aa3aae932e19d246
James Cook <jamescook.106@gmail.com>
2016-10-17 18:43:31 +0100

    Basic show static

--- app/controllers/general_controller.rb
+++ app/controllers/general_controller.rb
@@ -1,4 +1,16 @@
 class GeneralController < ApplicationController
+
   def static
+    if valid_page?
+      render template: "general/#{params[:page]}"
+    else
+      render file: "public/404.html", status: :not_found
+    end
   end
+
+  #Method for seeing if page exists
+  def valid_page?
+    File.exist?(Pathname.new(Rails.root + "app/views/general/#{params[:page]}.html.erb"))
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  get 'general/static'
 
+  get "/:page" => "general#static"
+
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+  root 'general#static', page: "index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
