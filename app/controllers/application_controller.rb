aaad8d18f86b2cd13d4c70f75d054842b3740030
James Cook <jamescook.106@gmail.com>
2016-10-25 19:41:10 +0100

    CanCan exception not working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   #If someone tries to access the admin without authorisation
   #then we redirect to the home page
-  protect_from_forgery with: :exception
-    rescue_from CanCan::AccessDenied do |exception|
-    redirect_to main_app.root_url
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to main_app.root_url, :alert => exception.message
   end
 end
