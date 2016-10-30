fc38e29107406a7a8124bdb918a4ef9c80d8b050
James Cook <jamescook.106@gmail.com>
2016-10-30 13:23:46 +0000

    Added curated to resources

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161030130231) do
+ActiveRecord::Schema.define(version: 20161030132301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,12 +34,13 @@ ActiveRecord::Schema.define(version: 20161030130231) do
     t.text     "name"
     t.text     "short"
     t.text     "long"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
     t.string   "file_file_name"
     t.string   "file_content_type"
     t.integer  "file_file_size"
     t.datetime "file_updated_at"
+    t.boolean  "currated",          default: false
   end
 
   create_table "users", force: :cascade do |t|
