c0ac7138458fb111ad45a26ce988f77cddeb85fb
James Cook <jamescook.106@gmail.com>
2016-10-30 13:19:13 +0000

    Added motivation, country and organsiation to the sign_up form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   #If someone tries to access the admin without authorisation
   #then we redirect to the home page
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.root_url, :alert => exception.message
   end
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:country, :motivation, :organisation])
+    end
 end
