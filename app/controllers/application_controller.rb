3aede30381da81ea705737d6edb861ead8234ad8
James Cook <jamescook.106@gmail.com>
2016-11-02 21:19:46 +0000

    Added some multilanguage Support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_filter :set_locale
+
   #If someone tries to access the admin without authorisation
   #then we redirect to the home page
-
-  before_filter :set_locale
-  def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
-  end
-
-  def default_url_options(options={})
-  { :locale => I18n.locale }
-end
-
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.root_url, :alert => exception.message
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 
   protected
