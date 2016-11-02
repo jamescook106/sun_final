a9842eef21ba7f6d6e364e88fb079c3657d37979
James Cook <jamescook.106@gmail.com>
2016-11-02 18:50:53 +0000

    Added validation for language and content type to resources

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -6,6 +6,8 @@ class Resource < ActiveRecord::Base
   validates :name, presence:true, length: { minimum: 2 }
   validates :short, presence:true
   validates :long, presence:true
+  validates :language, presence:true
+  validates :contenttype, presence:true
 
   has_attached_file :file
   do_not_validate_attachment_file_type :file
