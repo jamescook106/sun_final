5d2e67bf4842d670a087f721365752a8e188e5d2
James Cook <jamescook.106@gmail.com>
2016-12-10 12:53:44 +0000

    Added tests for default protection and featured for resources

--- test/models/resource_test.rb
+++ test/models/resource_test.rb
@@ -16,4 +16,31 @@ class ResourceTest < ActiveSupport::TestCase
     resource.long = 'Test Long'
     assert resource.valid?
   end
+
+  test "Resource featured default is false" do
+    resource=Resource.new
+    resource.name='Test'
+    resource.short = 'Test Short'
+    resource.long = 'Test Long'
+    resource.save
+    if resource.featured==false
+      assert true, 'Resource featured default is false'
+    else
+      assert false, 'Resource featured default is true'
+    end
+  end
+
+  test "Resource protected default is false" do
+    resource=Resource.new
+    resource.name='Test'
+    resource.short = 'Test Short'
+    resource.long = 'Test Long'
+    resource.save
+    if resource.protected==false
+      assert true, 'Resource protected default is false'
+    else
+      assert false, 'Resource protected default is true'
+    end
+  end
+
 end
