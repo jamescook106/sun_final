5ff30e2eb76e17379e1f58a348a223761df7fdc5
James Cook <jamescook.106@gmail.com>
2016-12-13 14:22:23 +0000

    Paginate to search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,10 +3,10 @@ class SearchController < ApplicationController
   def index
     @resources = Resource.all
     if params[:search] or params[:language] or params[:contenttype] or params[:content_tag] or params[:thematic_tag]
-      @resources = Resource.search(params[:search],params[:language],params[:contenttype],params[:content_tag],params[:thematic_tag])
+      @resources = Resource.search(params[:search],params[:language],params[:contenttype],params[:content_tag],params[:thematic_tag]).page(params[:page]).per(5)
     else
       @resources = Resource.all
     end
   end
-  
+
 end
