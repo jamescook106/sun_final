e8e929bd2757ebde4e7cda64e4100a1781ff4ed0
James Cook <jamescook.106@gmail.com>
2016-12-13 14:27:41 +0000

    Show all resources button

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,7 +5,7 @@ class SearchController < ApplicationController
     if params[:search] or params[:language] or params[:contenttype] or params[:content_tag] or params[:thematic_tag]
       @resources = Resource.search(params[:search],params[:language],params[:contenttype],params[:content_tag],params[:thematic_tag]).page(params[:page]).per(5)
     else
-      @resources = Resource.all
+      @resources = Resource.all.page(params[:page]).per(5)
     end
   end
 
