ff4aebb72bb0763ec7bfab07b870173f96b84303
James Cook <jamescook.106@gmail.com>
2017-05-18 11:04:16 +0100

    DOB and Network Removed from sign up form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,8 +21,8 @@ class ApplicationController < ActionController::Base
 
   protected
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:country, :motivation, :organisation, :name, :surname, :newsletter, :gender, :dob, :sunorg, :msun])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:country, :motivation, :organisation, :name, :surname, :newsletter, :gender, :dob, :sunorg, :msun])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:country, :motivation, :organisation, :name, :surname, :newsletter, :gender,:msun])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:country, :motivation, :organisation, :name, :surname, :newsletter, :gender,:msun])
 
     end
 
